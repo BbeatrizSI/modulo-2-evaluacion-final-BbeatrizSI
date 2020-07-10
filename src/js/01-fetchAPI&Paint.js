28e9e5e6f550291736e1108f1e13c052fb702cf4
BbeatrizSI <bbeatriz@gmail.com>
2020-07-10 13:16:35 +0200

    make cardShows change styles when clicked

--- src/js/01-fetchAPI&Paint.js
+++ src/js/01-fetchAPI&Paint.js
@@ -1,17 +1,24 @@
 'use strict';
 
+// Selectores Botón, Input, Sección de búsqueda y Sección de favoritos. Variables para Shows encontrados y para favoritos.
+
 const searchBtn = document.querySelector('.js-search-button');
 const searchInput = document.querySelector('.js-search-input');
 const favoriteSection = document.querySelector('.js-favorite-section');
 const searchSection = document.querySelector('.js-search-section');
 
-function getShowsInfo() {
+let searchedShows = [];
+let favoriteShows = [];
+
+// Función que pinta las tarjetas que se obtienen de la búsqueda con su listener sobre el Botón
+
+function paintShowCards() {
   fetch(`http://api.tvmaze.com/search/shows?q=${searchInput.value}`)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
         const divShow = document.createElement('div');
-        divShow.className = 'cardShow';
+        divShow.className = 'cardShow js-cardShow';
         const imgShow = document.createElement('img');
         const titleShow = document.createElement('h2');
 
@@ -33,9 +40,21 @@ function getShowsInfo() {
         divShow.appendChild(titleShow);
 
         searchSection.appendChild(divShow);
-        console.log(divShow);
+
+        //Convierto el NodeList que se me genera en un array
+        searchedShows = [...document.querySelectorAll('.js-cardShow')];
+      }
+
+      //Esta función es para aplicarle la clase de seleccionado a las tarjetas al clickearlas
+
+      function selectShow(ev) {
+        ev.currentTarget.classList.toggle('cardShow-selected');
+      }
+
+      for (let i = 0; i < searchedShows.length; i++) {
+        searchedShows[i].addEventListener('click', selectShow);
       }
     });
 }
 
-searchBtn.addEventListener('click', getShowsInfo);
+searchBtn.addEventListener('click', paintShowCards);
