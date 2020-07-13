35ab71ead6c0addb064a9f3adc221726f3865d89
BbeatrizSI <bbeatriz@gmail.com>
2020-07-13 08:44:24 +0200

    fix delete all button

--- src/js/04-deleteFavorites.js
+++ src/js/04-deleteFavorites.js
@@ -2,28 +2,29 @@
 
 /* BOTÓN DE CLEAR Y "X" DE CADA SERIE FAVORITA */
 
-// crear botón de limpiar favoritos cuando la sección de favoritos tiene contenido
+// crear botón de limpiar favoritos cuando la sección de favoritos tiene contenido y escucharlo.
 
 function createDeleteBtn() {
   let codeHTML =
     '<button id="delete-fav-button" class="delete-fav-button js-deleteAllFav" type="button">Clear</button>';
   containerDeleteAllBtn.innerHTML = codeHTML;
+
+  const deleteAllBtn = document.querySelector('.js-deleteAllFav');
+  deleteAllBtn.addEventListener('click', clearAllFavorites);
 }
 
 // función para borrar todos los favoritos, tanto del local storage como del array de favoritos. También quita la clase de seleccionado a todo y vacía el aside donde se pintan.
 
 function clearAllFavorites() {
-  localStorage.removeItem('favoriteList');
   for (let i = 0; i < cardShows.length; i++) {
     cardShows[i].classList.remove('cardShow-selected');
   }
+  console.log('llego aquí');
   favoriteListSection.innerHTML = '';
   containerDeleteAllBtn.innerHTML = '';
   favoriteShows.splice(0, favoriteShows.length);
+  localStorage.removeItem('favoriteList');
 }
-
-const deleteAllBtn = document.querySelector('.js-deleteAllFav');
-deleteAllBtn.addEventListener('click', clearAllFavorites);
 
 // habilitar las "X" para que eliminen del array de favoritos y del LocalStorage una a una las series. También desaparecen del array y se le quita la clase de seleccionada.
 
